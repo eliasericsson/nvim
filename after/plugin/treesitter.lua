e96d056fb0452c306ac8c9e5afbfac1534d417b7
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2023-01-31 13:47:03 +0100

    Add python

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "javascript", "typescript", "lua", "rust", "kotlin", "terraform", "yaml", },
+  ensure_installed = { "c", "javascript", "typescript", "lua", "rust", "kotlin", "terraform", "yaml", "python"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
