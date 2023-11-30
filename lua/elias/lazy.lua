5d0ea32e3db70c87fe85479469cafec82c5e10a6
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2023-11-30 13:22:44 +0100

    Remove commented config

--- lua/elias/lazy.lua
+++ lua/elias/lazy.lua
@@ -231,28 +231,6 @@ require("lazy").setup({
             end
         }
     },
-    -- {
-    --     "VonHeikemen/lsp-zero.nvim",
-    --     branch = "v3.x",
-    --     requires = {
-    --         -- LSP Support
-    --         "neovim/nvim-lspconfig",
-    --         "williamboman/mason.nvim",
-    --         "williamboman/mason-lspconfig.nvim",
-
-    --         -- Autocompletion
-    --         "hrsh7th/nvim-cmp",
-    --         "hrsh7th/cmp-buffer",
-    --         "hrsh7th/cmp-path",
-    --         "hrsh7th/cmp-nvim-lsp",
-    --         "hrsh7th/cmp-nvim-lua",
-
-    --         -- Snippets
-    --         "L3MON4D3/LuaSnip",
-    --         "rafamadriz/friendly-snippets",
-    --         "SirVer/ultisnips",
-    --     },
-    -- },
 
     -- Markdown Preview
     {
