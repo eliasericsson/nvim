13b14d88c3d93e3b49a4141e8366cabb6f363be5
Elias Ericsson-Rydberg <elias@playgroundtech.io>
2025-02-12 13:13:23 +0100

    feat(snippets,cron): add snippets and cronex
    faster creation of gitbook hints and information about cron job configs

--- snippets/all.lua
+++ snippets/all.lua
@@ -2,4 +2,23 @@ return {
     s("gconv", {
         i(1, "type"), t("("), i(2, "scope"), t("): "), i(3, "description"),
     }),
+
+    s("cron", {
+        i(1, "minute"), t(" "), i(2, "hour"), t(" "), i(3, "day"), t(" "), i(4, "month"), t(" "), i(5, "weekday"), t(" "),
+        i(6, "user"), t(" "), i(7, "command"),
+    }),
+
+    s("hint",
+        fmt(
+            [[
+        {{% hint style="{1}" %}}
+        {2}
+        {{% endhint %}}
+            ]],
+            {
+                c(1, { t("info"), t("success"), t("warning"), t("danger") }),
+                i(2, "content"),
+            }
+        )
+    ),
 }
